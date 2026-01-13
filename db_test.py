1d6a7ca97f4fef32b1630524bb1ff94baaabfc6d
waziri <olugbadewaziri99@gmail.com>
2026-01-13 23:11:14 +0100

    New Fix!

--- db_test.py
+++ db_test.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 
-# Replace [YOUR-PASSWORD] with your actual password
+# Try the new password here
+# Note the 'postgresql' at the start!
 DB_URI = "postgresql://postgres:vsOHLeWizGp6eYer@db.mmbuabemfkxdavsaaccq.supabase.co:5432/postgres"
 
 try:
     engine = create_engine(DB_URI)
-    connection = engine.connect()
-    print("🚀 SUCCESS: Python is talking to the Database!")
-    connection.close()
+    with engine.connect() as conn:
+        print("✅ CONNECTION SUCCESSFUL!")
 except Exception as e:
-    print(f"❌ CONNECTION FAILED: {e}")
+    print(f"❌ ERROR: {e}")
